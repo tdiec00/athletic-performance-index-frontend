f7ae4efa51cbfeebd1e1f016c3e8ad02c6076cbb
Trung Diec <tdiec00@gmail.com>
2022-03-09 20:40:53 -0500

    schema updates

--- src/models/tracker.js
+++ src/models/tracker.js
@@ -4,7 +4,7 @@ import exerciseSchema from "./exercise"
 
 const trackerSchema = new Schema(
   {
-    username: {type: String, required: true, trim: true},
+    user: {type: mongoose.Schema.Types.ObjectId, ref: "User", trim: true},
     exercises: [exerciseSchema],
   },
   {timestamps: true}
